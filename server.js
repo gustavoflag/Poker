e0f459fd4ba02858725870832d1c04c9f1c48698
Gustavo Bandeira <gustavoflag@yahoo.com.br>
2017-12-20 22:36:03 -0200

    Alteração no controle de acesso (Header)

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/pokerRoutes');
 
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT'){
     jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode){
       if (err)
