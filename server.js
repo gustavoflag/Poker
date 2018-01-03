f75f8468bdb0c315261b8ae274ed0169f9abdefb
Gustavo Bandeira <gustavoflag@yahoo.com.br>
2018-01-03 19:01:48 -0200

    .gitignore criado para não deixar subir /node_modules

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/poker');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/poker');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
