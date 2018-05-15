99f13959924aa13258181d34b6221753bfa943ef
Gustavo Bandeira <gustavoflag@yahoo.com.br>
2018-05-15 16:23:09 -0300

    Refactory das funções de ordenação - campeonato

--- api/controllers/jogadorController.js
+++ api/controllers/jogadorController.js
@@ -1,6 +1,7 @@
 var sortBy = require('sort-by');
-var mongoose = require('mongoose'),
-Jogador = mongoose.model('Jogador'),
+var mongoose = require('mongoose');
+var jogadorOrders = require('../helpers/jogadorOrders.js');
+Jogador = mongoose.model('Jogador');
 Jogo = mongoose.model('Jogo');
 
 exports.listar = function(req, res) {
@@ -8,34 +9,6 @@ exports.listar = function(req, res) {
     if (err)
       return res.status(440).json(err);
     return res.json(jogadores.sort(sortBy('nome')));
-  });
-};
-
-function classificacaoGeral(ordem, callback){
-  Jogador.find({}, function(err, jogadores) {
-    if (err)
-      callback(err, null);
-
-    jogadores.forEach(jogador => getEstatisticasJogador(jogador));
-
-    switch(ordem){
-      case "P":
-        callback(null, jogadores.sort(compararJogadores));
-        break;
-      case "V":
-        callback(null, jogadores.sort(compararVitorias));
-        break;
-      case "PPJ":
-        callback(null, jogadores.sort(compararPontosPorJogo));
-        break;
-      case "VR":
-        callback(null, jogadores.sort(compararValorRecebido));
-        break;
-      case "S":
-        callback(null, jogadores.sort(compararSaldo));
-        break;
-    }
-
   });
 };
 
@@ -94,6 +67,80 @@ exports.classificacaoTodosMeses = function(req, res){
   }
 };
 
+exports.inserir = function(req, res) {
+  var novoJogador = new Jogador(req.body);
+
+  novoJogador.historicoJogos = [];
+  novoJogador.historicoJogos.push({ lugar:-1, quantidade:0 });
+
+  for (var i = 1; i <= 15; i++){
+    novoJogador.historicoJogos.push({ lugar:i, quantidade:0 });
+  }
+
+  novoJogador.save(function(err, task) {
+    if (err)
+      return res.status(440).json(err);
+    return res.json(task);
+  });
+};
+
+exports.consultar = function(req, res) {
+  Jogador.findById(req.params.jogadorId, function(err, jogador) {
+    if (err)
+      return res.status(440).json(err);
+
+    classificacaoGeral("P", function(err, jogadores){
+      if (err)
+        return res.status(440).json(err);
+
+      getEstatisticasJogador(jogador);
+
+      //posicões relativas ao ranking
+      jogador.posicaoRanking = encontraPosicao(jogadores, jogador.nome);
+      jogador.posicaoVitorias = encontraPosicao(jogadores.sort(jogadorOrders.compararVitorias), jogador.nome);
+      jogador.posicaoHU = encontraPosicao(jogadores.sort(jogadorOrders.compararHUs), jogador.nome);
+      jogador.posicaoPontuacoes = encontraPosicao(jogadores.sort(jogadorOrders.compararPontuacoes), jogador.nome);
+      jogador.posicaoPontosPorJogo = encontraPosicao(jogadores.sort(jogadorOrders.compararPontosPorJogo), jogador.nome);
+      jogador.posicaoMediaPosicao = encontraPosicao(jogadores.sort(jogadorOrders.compararMediaPosicao), jogador.nome);
+
+      Jogo.find({ participantes: {$elemMatch: { nomeJogador: jogador.nome, lugar: 1 }}}, function(err, jogos){
+        if (err)
+          return res.status(440).json(err);
+
+        jogos.forEach((jogo) => {
+          var resumoJogo = {
+            _id: jogo._id,
+            data: jogo.data
+          };
+
+          jogador.vitorias.push(resumoJogo);
+        });
+
+        return res.json(jogador);
+      });
+    });
+  });
+};
+
+exports.alterar = function(req, res) {
+  Jogador.findOneAndUpdate({_id: req.params.jogadorId}, req.body, {new: true}, function(err, task) {
+    if (err)
+      return res.status(440).json(err);
+    return res.json(task);
+  });
+};
+
+exports.excluir = function(req, res) {
+  Jogador.remove({
+    _id: req.params.jogadorId
+  }, function(err, task) {
+    if (err)
+      return res.status(440).json(err);
+    return res.json({ message: 'Jogador excluído' });
+  });
+};
+
+//Funções
 function classMes(ano, mes, callback){
   var jogadores = [];
 
@@ -148,6 +195,64 @@ function classMes(ano, mes, callback){
   });
 };
 
+function classificacaoGeral(ordem, callback){
+  Jogador.find({}, function(err, jogadores) {
+    if (err)
+      callback(err, null);
+
+    jogadores.forEach(jogador => getEstatisticasJogador(jogador));
+
+    switch(ordem){
+      case "P":
+        callback(null, jogadores.sort(jogadorOrders.compararJogadores));
+        break;
+      case "V":
+        callback(null, jogadores.sort(jogadorOrders.compararVitorias));
+        break;
+      case "PPJ":
+        callback(null, jogadores.sort(jogadorOrders.compararPontosPorJogo));
+        break;
+      case "VR":
+        callback(null, jogadores.sort(jogadorOrders.compararValorRecebido));
+        break;
+      case "S":
+        callback(null, jogadores.sort(jogadorOrders.compararSaldo));
+        break;
+    }
+
+  });
+};
+
+function getEstatisticasJogador(jogador){
+  jogador.pontosPorJogo = jogador.pontos / jogador.jogos;
+
+  jogador.qtdHUs = 0;
+  jogador.qtdPontuacoes = 0;
+  jogador.mediaPosicao = 0;
+  jogador.qtdVitorias = 0;
+
+  jogador.historicoJogos.forEach((historico) => {
+    if (historico.lugar <= 6 && historico.lugar > 0){
+      jogador.qtdPontuacoes += historico.quantidade;
+    }
+    if (historico.lugar <= 2 && historico.lugar > 0){
+      jogador.qtdHUs += historico.quantidade;
+    }
+    if (historico.lugar == 1){
+      jogador.qtdVitorias += historico.quantidade;
+    }
+    if (historico.lugar > 0){
+      jogador.mediaPosicao += (historico.lugar * historico.quantidade);
+    }
+  });
+
+  jogador.mediaPosicao = jogador.mediaPosicao / jogador.jogos;
+};
+
+function encontraPosicao(jogadores, nomeJogador){
+  return (jogadores.findIndex(j => j.nome == nomeJogador) + 1);
+};
+
 function nomeMes(indice, callback){
     var nomeMesI;
 
@@ -191,250 +296,3 @@ function nomeMes(indice, callback){
 
     return callback(nomeMesI);
 }
-
-function compararPontos(a, b){
-  var diffPontos = (a.pontos - b.pontos);
-  if (diffPontos != 0){
-    return diffPontos * -1;
-  }
-}
-
-function compararVitorias(a, b){
-  var diffVitorias = (a.qtdVitorias - b.qtdVitorias);
-  if (diffVitorias != 0){
-    return diffVitorias * -1;
-  }
-
-  var diffPontos = (a.pontos - b.pontos);
-  if (diffPontos != 0){
-    return diffPontos * -1;
-  }
-
-  var diffJogos = (a.jogos - b.jogos);
-  if (diffJogos != 0){
-    return diffJogos;
-  }
-}
-
-function compararHUs(a, b){
-  var diffHUs = (a.qtdHUs - b.qtdHUs);
-  if (diffHUs != 0){
-    return diffHUs * -1;
-  }
-}
-
-function compararSaldo(a, b){
-  var diffSaldo = ((a.valorRecebido - a.valorInvestido) - (b.valorRecebido - b.valorInvestido));
-  if (diffSaldo != 0){
-    return diffSaldo * -1;
-  }
-
-  var diffPontos = (a.pontos - b.pontos);
-  if (diffPontos != 0){
-    return diffPontos * -1;
-  }
-
-  var diffVitorias = (a.qtdVitorias - b.qtdVitorias);
-  if (diffVitorias != 0){
-    return diffVitorias * -1;
-  }
-
-  var diffJogos = (a.jogos - b.jogos);
-  if (diffJogos != 0){
-    return diffJogos;
-  }
-}
-
-function compararValorRecebido(a, b){
-  var diffValorRecebido = (a.valorRecebido - b.valorRecebido);
-  if (diffValorRecebido != 0){
-    return diffValorRecebido * -1;
-  }
-
-  var diffPontos = (a.pontos - b.pontos);
-  if (diffPontos != 0){
-    return diffPontos * -1;
-  }
-
-  var diffVitorias = (a.qtdVitorias - b.qtdVitorias);
-  if (diffVitorias != 0){
-    return diffVitorias * -1;
-  }
-
-  var diffJogos = (a.jogos - b.jogos);
-  if (diffJogos != 0){
-    return diffJogos;
-  }
-}
-
-
-function compararPontosPorJogo(a, b){
-  var diffPontosPorJogo = (a.pontosPorJogo - b.pontosPorJogo);
-  if (diffPontosPorJogo != 0){
-    return diffPontosPorJogo * -1;
-  }
-
-  var diffPontos = (a.pontos - b.pontos);
-  if (diffPontos != 0){
-    return diffPontos * -1;
-  }
-
-  var diffVitorias = (a.qtdVitorias - b.qtdVitorias);
-  if (diffVitorias != 0){
-    return diffVitorias * -1;
-  }
-
-  var diffJogos = (a.jogos - b.jogos);
-  if (diffJogos != 0){
-    return diffJogos;
-  }
-}
-
-function compararMediaPosicao(a, b){
-  var diffMediaPosicao = (a.mediaPosicao - b.mediaPosicao);
-  if (diffMediaPosicao != 0){
-    return diffMediaPosicao;
-  }
-}
-
-function compararPontuacoes(a, b){
-  var diffPontuacoes = (a.qtdPontuacoes - b.qtdPontuacoes);
-  if (diffPontuacoes != 0){
-    return diffPontuacoes * -1;
-  }
-}
-
-function compararMeses(a, b){
-  var diffMeses = (a.mes - b.mes);
-  if (diffMeses != 0){
-    return diffMeses * -1;
-  }
-}
-
-function compararJogadores(a, b){
-  var diffPontos = (a.pontos - b.pontos);
-  if (diffPontos != 0){
-    return diffPontos * -1;
-  }
-
-  for (var i = 1; i <= 15; i++){
-    var diffPos = (a.historicoJogos[i].quantidade - b.historicoJogos[i].quantidade);
-
-    if (diffPos != 0){
-      return diffPos * -1;
-    }
-  }
-
-  var diffValor = (a.valorRecebido - b.valorRecebido);
-  if (diffValor != 0){
-    return diffValor * -1;
-  }
-
-  var diffJogos = (a.jogos - b.jogos);
-  if (diffJogos != 0){
-    return diffJogos;
-  }
-}
-
-exports.inserir = function(req, res) {
-  var novoJogador = new Jogador(req.body);
-
-  novoJogador.historicoJogos = [];
-  novoJogador.historicoJogos.push({ lugar:-1, quantidade:0 });
-
-  for (var i = 1; i <= 15; i++){
-    novoJogador.historicoJogos.push({ lugar:i, quantidade:0 });
-  }
-
-  novoJogador.save(function(err, task) {
-    if (err)
-      return res.status(440).json(err);
-    return res.json(task);
-  });
-};
-
-function getEstatisticasJogador(jogador){
-  jogador.pontosPorJogo = jogador.pontos / jogador.jogos;
-
-  jogador.qtdHUs = 0;
-  jogador.qtdPontuacoes = 0;
-  jogador.mediaPosicao = 0;
-  jogador.qtdVitorias = 0;
-
-  jogador.historicoJogos.forEach((historico) => {
-    if (historico.lugar <= 6 && historico.lugar > 0){
-      jogador.qtdPontuacoes += historico.quantidade;
-    }
-    if (historico.lugar <= 2 && historico.lugar > 0){
-      jogador.qtdHUs += historico.quantidade;
-    }
-    if (historico.lugar == 1){
-      jogador.qtdVitorias += historico.quantidade;
-    }
-    if (historico.lugar > 0){
-      jogador.mediaPosicao += (historico.lugar * historico.quantidade);
-    }
-  });
-
-  jogador.mediaPosicao = jogador.mediaPosicao / jogador.jogos;
-};
-
-function encontraPosicao(jogadores, nomeJogador){
-  return (jogadores.findIndex(j => j.nome == nomeJogador) + 1);
-};
-
-exports.consultar = function(req, res) {
-  Jogador.findById(req.params.jogadorId, function(err, jogador) {
-    if (err)
-      return res.status(440).json(err);
-
-    classificacaoGeral("P", function(err, jogadores){
-      if (err)
-        return res.status(440).json(err);
-
-      getEstatisticasJogador(jogador);
-
-      //posicões relativas ao ranking
-      jogador.posicaoRanking = encontraPosicao(jogadores, jogador.nome);
-      jogador.posicaoVitorias = encontraPosicao(jogadores.sort(compararVitorias), jogador.nome);
-      jogador.posicaoHU = encontraPosicao(jogadores.sort(compararHUs), jogador.nome);
-      jogador.posicaoPontuacoes = encontraPosicao(jogadores.sort(compararPontuacoes), jogador.nome);
-      jogador.posicaoPontosPorJogo = encontraPosicao(jogadores.sort(compararPontosPorJogo), jogador.nome);
-      jogador.posicaoMediaPosicao = encontraPosicao(jogadores.sort(compararMediaPosicao), jogador.nome);
-
-      Jogo.find({ participantes: {$elemMatch: { nomeJogador: jogador.nome, lugar: 1 }}}, function(err, jogos){
-        if (err)
-          return res.status(440).json(err);
-
-        jogos.forEach((jogo) => {
-          var resumoJogo = {
-            _id: jogo._id,
-            data: jogo.data
-          };
-
-          jogador.vitorias.push(resumoJogo);
-        });
-
-        return res.json(jogador);
-      });
-    });
-  });
-};
-
-exports.alterar = function(req, res) {
-  Jogador.findOneAndUpdate({_id: req.params.jogadorId}, req.body, {new: true}, function(err, task) {
-    if (err)
-      return res.status(440).json(err);
-    return res.json(task);
-  });
-};
-
-exports.excluir = function(req, res) {
-  Jogador.remove({
-    _id: req.params.jogadorId
-  }, function(err, task) {
-    if (err)
-      return res.status(440).json(err);
-    return res.json({ message: 'Jogador excluído' });
-  });
-};
---
+++ api/helpers/jogadorOrders.js
@@ -0,0 +1,144 @@
+//Ordenações
+exports.compararPontos = function(a, b){
+  var diffPontos = (a.pontos - b.pontos);
+  if (diffPontos != 0){
+    return diffPontos * -1;
+  }
+}
+
+exports.compararVitorias = function(a, b){
+  var diffVitorias = (a.qtdVitorias - b.qtdVitorias);
+  if (diffVitorias != 0){
+    return diffVitorias * -1;
+  }
+
+  var diffPontos = (a.pontos - b.pontos);
+  if (diffPontos != 0){
+    return diffPontos * -1;
+  }
+
+  var diffJogos = (a.jogos - b.jogos);
+  if (diffJogos != 0){
+    return diffJogos;
+  }
+}
+
+exports.compararHUs = function(a, b){
+  var diffHUs = (a.qtdHUs - b.qtdHUs);
+  if (diffHUs != 0){
+    return diffHUs * -1;
+  }
+}
+
+exports.compararSaldo = function(a, b){
+  var diffSaldo = ((a.valorRecebido - a.valorInvestido) - (b.valorRecebido - b.valorInvestido));
+  if (diffSaldo != 0){
+    return diffSaldo * -1;
+  }
+
+  var diffPontos = (a.pontos - b.pontos);
+  if (diffPontos != 0){
+    return diffPontos * -1;
+  }
+
+  var diffVitorias = (a.qtdVitorias - b.qtdVitorias);
+  if (diffVitorias != 0){
+    return diffVitorias * -1;
+  }
+
+  var diffJogos = (a.jogos - b.jogos);
+  if (diffJogos != 0){
+    return diffJogos;
+  }
+}
+
+exports.compararValorRecebido = function(a, b){
+  var diffValorRecebido = (a.valorRecebido - b.valorRecebido);
+  if (diffValorRecebido != 0){
+    return diffValorRecebido * -1;
+  }
+
+  var diffPontos = (a.pontos - b.pontos);
+  if (diffPontos != 0){
+    return diffPontos * -1;
+  }
+
+  var diffVitorias = (a.qtdVitorias - b.qtdVitorias);
+  if (diffVitorias != 0){
+    return diffVitorias * -1;
+  }
+
+  var diffJogos = (a.jogos - b.jogos);
+  if (diffJogos != 0){
+    return diffJogos;
+  }
+}
+
+
+exports.compararPontosPorJogo = function(a, b){
+  var diffPontosPorJogo = (a.pontosPorJogo - b.pontosPorJogo);
+  if (diffPontosPorJogo != 0){
+    return diffPontosPorJogo * -1;
+  }
+
+  var diffPontos = (a.pontos - b.pontos);
+  if (diffPontos != 0){
+    return diffPontos * -1;
+  }
+
+  var diffVitorias = (a.qtdVitorias - b.qtdVitorias);
+  if (diffVitorias != 0){
+    return diffVitorias * -1;
+  }
+
+  var diffJogos = (a.jogos - b.jogos);
+  if (diffJogos != 0){
+    return diffJogos;
+  }
+}
+
+exports.compararMediaPosicao = function(a, b){
+  var diffMediaPosicao = (a.mediaPosicao - b.mediaPosicao);
+  if (diffMediaPosicao != 0){
+    return diffMediaPosicao;
+  }
+}
+
+exports.compararPontuacoes = function(a, b){
+  var diffPontuacoes = (a.qtdPontuacoes - b.qtdPontuacoes);
+  if (diffPontuacoes != 0){
+    return diffPontuacoes * -1;
+  }
+}
+
+exports.compararMeses = function(a, b){
+  var diffMeses = (a.mes - b.mes);
+  if (diffMeses != 0){
+    return diffMeses * -1;
+  }
+}
+
+exports.compararJogadores = function(a, b){
+  var diffPontos = (a.pontos - b.pontos);
+  if (diffPontos != 0){
+    return diffPontos * -1;
+  }
+
+  for (var i = 1; i <= 15; i++){
+    var diffPos = (a.historicoJogos[i].quantidade - b.historicoJogos[i].quantidade);
+
+    if (diffPos != 0){
+      return diffPos * -1;
+    }
+  }
+
+  var diffValor = (a.valorRecebido - b.valorRecebido);
+  if (diffValor != 0){
+    return diffValor * -1;
+  }
+
+  var diffJogos = (a.jogos - b.jogos);
+  if (diffJogos != 0){
+    return diffJogos;
+  }
+}
