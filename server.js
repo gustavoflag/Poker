2e470103a433479804e130fb4e97fc6f33d32203
Gustavo Lombardi <gustavolomabardi@WP061760NRJBA.local>
2020-08-28 00:23:09 -0300

    Ajuste netlify

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const router = express.Router();
+const routes = require('./api/routes/pokerRoutes');
+//const port = process.env.PORT || 3000,
+const mongoose = require('mongoose'),
   Pontuacao = require('./api/models/pontuacao'),
   Premiacao = require('./api/models/premiacao'),
   Jogador = require('./api/models/jogador'),
@@ -18,13 +21,11 @@ mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/poker'); //local
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_b595dzsg:8qcrfj4oqhv4q8pkjs4iiv7nfm@cluster-b595dzsg-shard-00-00.wqxdy.mongodb.net:27017,cluster-b595dzsg-shard-00-01.wqxdy.mongodb.net:27017,cluster-b595dzsg-shard-00-02.wqxdy.mongodb.net:27017/heroku_b595dzsg?ssl=true&replicaSet=atlas-131hw6-shard-0&authSource=admin&retryWrites=true&w=majority'); //2018 - Migrado
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_1h8pfvcr:e5r69cvt2q9qfphibu5mjiej40@cluster-1h8pfvcr-shard-00-00.rnhd2.mongodb.net:27017,cluster-1h8pfvcr-shard-00-01.rnhd2.mongodb.net:27017,cluster-1h8pfvcr-shard-00-02.rnhd2.mongodb.net:27017/heroku_1h8pfvcr?ssl=true&replicaSet=atlas-ag1wet-shard-0&authSource=admin&retryWrites=true&w=majority'); //2019 - Migrado
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_5lgwrn5j:6fq01flrm14o51em30nk9nibob@cluster-5lgwrn5j-shard-00-00.ayh0m.mongodb.net:27017,cluster-5lgwrn5j-shard-00-01.ayh0m.mongodb.net:27017,cluster-5lgwrn5j-shard-00-02.ayh0m.mongodb.net:27017/heroku_5lgwrn5j?ssl=true&replicaSet=atlas-b0oga1-shard-0&authSource=admin&retryWrites=true&w=majority'); //STG - Migrado
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_5lgwrn5j:6fq01flrm14o51em30nk9nibob@cluster-5lgwrn5j-shard-00-00.ayh0m.mongodb.net:27017,cluster-5lgwrn5j-shard-00-01.ayh0m.mongodb.net:27017,cluster-5lgwrn5j-shard-00-02.ayh0m.mongodb.net:27017/heroku_5lgwrn5j?ssl=true&replicaSet=atlas-b0oga1-shard-0&authSource=admin&retryWrites=true&w=majority'); //STG - Migrado
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_dvvkpq45:6a04qh41uh1tgvk0dcgl3mhl4j@ds119578.mlab.com:19578/heroku_dvvkpq45'); //2020
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_dvvkpq45:6a04qh41uh1tgvk0dcgl3mhl4j@cluster-dvvkpq45-shard-00-00.togo7.mongodb.net:27017,cluster-dvvkpq45-shard-00-01.togo7.mongodb.net:27017,cluster-dvvkpq45-shard-00-02.togo7.mongodb.net:27017/heroku_dvvkpq45?ssl=true&replicaSet=atlas-cl7yo6-shard-0&authSource=admin&retryWrites=true&w=majority'); //2020 - Migrado
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_dvvkpq45:6a04qh41uh1tgvk0dcgl3mhl4j@cluster-dvvkpq45-shard-00-00.togo7.mongodb.net:27017,cluster-dvvkpq45-shard-00-01.togo7.mongodb.net:27017,cluster-dvvkpq45-shard-00-02.togo7.mongodb.net:27017/heroku_dvvkpq45?ssl=true&replicaSet=atlas-cl7yo6-shard-0&authSource=admin&retryWrites=true&w=majority'); //2020 - Migrado
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-var routes = require('./api/routes/pokerRoutes');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -43,8 +44,16 @@ app.use(function(req, res, next) {
   }
 });
 
-routes(app);
+routes(router);
 
-app.listen(port);
+app.use('/.netlify/functions/server', router);
 
-console.log('API iniciada, porta: ' + port);
+module.exports.handler = serverless(app);
+
+//var routes = require('./api/routes/pokerRoutes');
+
+//routes(app);
+
+// app.listen(port);
+
+// console.log('API iniciada, porta: ' + port);
