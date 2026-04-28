a18702a4fb23413ecd1b483e7ce54079a02d09f1
Gustavo Bandeira <gustavoflag@yahoo.com.br>
2026-04-28 20:13:43 -0300

    hotfix prod

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT'){
-    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode){
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET || "RESTFULAPIs", function(err, decode){
       if (err)
         req.user = undefined;
       req.user = decode;
